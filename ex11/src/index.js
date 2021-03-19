0b2833e83ee3bd76f55354349a05dca4bb4f63e0
krdzex <58869690+krdzex@users.noreply.github.com>
2021-03-19 10:45:01 +0100

    Small change

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -15,5 +15,5 @@ console.log(splitArrayInGroups(["a", "b", "c", "d"], 2));
 console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
 console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5], 2));
 console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 3));
-console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6], 2));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
 module.exports = splitArrayInGroups;
