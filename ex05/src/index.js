ceec50ab7e5855d6ac395205b1b93a6879e84ab2
krdzex <58869690+krdzex@users.noreply.github.com>
2021-03-21 22:33:25 +0100

    Update index.js

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,8 +1,7 @@
 // Only change code below this line
 var myNestedArray = [["Toblerone", 5], ["Milka", 3]];
 function myNestedFunction(arr) {
-    myNestedArray = [...arr];
-    return myNestedArray;
+    return arr;
 }
 // Only change code above this line
 console.log(myNestedFunction(myNestedArray)); // Change this line
